acaf4032077182380047ae43dae7ed946a0ca0c0
sandeep poomanglaath <sandeeppoomanglaath@sandeeps-MacBook-Pro.local>
2024-08-10 16:05:09 +0800

    Update code for spring

---
+++ spring/src/main/java/com/app/App.java
@@ -0,0 +1,54 @@
+package com.app;
+
+import com.holder.Holder;
+import com.model.Mapping;
+import com.model.Request;
+import com.utility.Utils;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.lang.reflect.InvocationTargetException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class App {
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(5001);
+        Holder holder = Holder.getInstance();
+        holder.scan();
+
+        while (true) {
+            Socket socket = serverSocket.accept();
+            BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            Request request = null;
+            try {
+                request = Utils.getRequest(br);
+
+//				System.out.println(request.getBody());
+            } catch (Exception e) {
+                System.out.println("Utility Exception...."+e);
+            }
+
+            System.out.println(request.getPath());
+            Mapping m = holder.getMapping().get(request.getPath());
+            String actualResult = "";
+            try {
+                actualResult = (String)m.getMethod().invoke(m.getObject(), request.getBody());
+            } catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+
+            PrintWriter out = new PrintWriter(socket.getOutputStream());
+
+            // we send HTTP Headers with data to client
+            Utils.writeResponse(out, actualResult);
+
+            socket.close();
+
+
+        }
+    }
+}
---
+++ spring/src/main/java/com/model/Request.java
@@ -0,0 +1,45 @@
+package com.model;
+
+import java.util.Map;
+
+public class Request {
+    private String method;
+    private String path;
+    private Map<String, String> headers;
+    private Map<String, String> queryParams;
+    private String body;
+
+    public Request(String method, String path, Map<String, String> headers, Map<String, String> queryParams, String body) {
+        this.method = method;
+        this.path = path;
+        this.headers = headers;
+        this.queryParams = queryParams;
+        this.body = body;
+    }
+
+    public String getMethod() {
+        return method;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public Map<String, String> getHeaders() {
+        return headers;
+    }
+
+    public Map<String, String> getQueryParams() {
+        return queryParams;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    @Override
+    public String toString() {
+        String result = "method: "+ method+" path: "+path;
+        return result;
+    }
+}
